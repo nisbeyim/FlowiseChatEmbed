dce4b12fbb4ee02398b47c8932ceaf04c4876d8c
twighlock <joyfuldefender@gmail.com>
2023-10-11 09:50:38 +0600

    Change link from app.nis.edu.kz to app.beyim.ai

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://app.beyim.nis.edu.kz'}
+        href={'https://app.beyim.ai/'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
